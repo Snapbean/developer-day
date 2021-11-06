1c9e743797e8f102d1a5384fcb9f9a5191a0847e
Dani Meier <dani.meier@gmail.com>
2021-11-06 10:57:27 +0100

    Add initial content to git-repos

--- git/setup.sh
+++ git/setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-
-echo 'Create application "devday-git-1"'
-dotnet new console -n devday-git-1
-echo 'Create application "devday-git-2"'
-dotnet new console -n devday-git-2
-
 echo 'Create repository for "devday-git-1"'
-cd ./devday-git-1
 gh repo create Snapbean/devday-git-1 \
 	--enable-issues=false --enable-wiki=false \
-	--public --gitignore=VisualStudio -y
+	--public --gitignore=VisualStudio --license=MIT -y
+
+echo 'Create application "devday-git-1"'
+cd ./devday-git-1
+dotnet new console
+git add .
+git commit -m 'Initial commit'
+git push -u origin main
+cd ..
 
 echo 'Create repository for "devday-git-2"'
-cd ../devday-git-2
 gh repo create Snapbean/devday-git-2 \
 	--enable-issues=false --enable-wiki=false \
-	--public --gitignore=VisualStudio -y
+	--public --gitignore=VisualStudio --license=MIT -y
 
+echo 'Create application "devday-git-2"'
+cd ./devday-git-2
+dotnet new console
+git add .
+git commit -m 'Initial commit'
+git push -u origin main
 cd ..
--- git/teardown.sh
+++ git/teardown.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf devday-git-1 devday-git-2
 gh repo delete Snapbean/devday-git-1 --confirm
 gh repo delete Snapbean/devday-git-2 --confirm
