0294ea7feaeb35dc2eef6c0e9ca6fb45363ff002
Manuel Sidler <manuel.sidler@outlook.com>
2021-11-14 16:29:37 +0100

    add hot reload examples

---
+++ hotreload/HotReloadWpf/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace HotReloadWpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClickMeButton.Content = "Got clicked";
+        }
+    }
+}
